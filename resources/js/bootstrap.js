935263f244d6e11457e33f44a77d6d9b4bf9f2c4
Meiy <donmeiyarasan@gmail.com>
2026-09-13 00:38:33 +0530

    feat: add bootstrap JS configuration and meeting-show Livewire view with interactive tools

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -14,3 +14,43 @@ window.$alert = window.$alert || function(options, type = 'info') {
     window.dispatchEvent(new CustomEvent('alert', { detail }));
 };
 
+// Route standard native window.alert to custom alert modal
+window.alert = function(message) {
+    if (typeof window.$alert === 'function') {
+        window.$alert(message);
+    }
+};
+
+// Intercept Livewire wire:confirm directive globally so any wire:confirm uses the custom alert modal
+document.addEventListener('livewire:init', () => {
+    if (window.Livewire && window.Livewire.directive) {
+        window.Livewire.directive('confirm', ({ el, directive }) => {
+            let message = directive.expression;
+            message = (message || '').replaceAll('\\n', '\n');
+            if (!message) message = 'Are you sure you want to proceed?';
+
+            el.__livewire_confirm = (action, instead) => {
+                if (typeof window.$confirm === 'function') {
+                    window.$confirm({
+                        title: 'Please Confirm',
+                        message: message,
+                        type: 'warning',
+                        confirmText: 'Confirm',
+                        cancelText: 'Cancel',
+                        onConfirm: action
+                    }).then(confirmed => {
+                        if (!confirmed && typeof instead === 'function') {
+                            instead();
+                        }
+                    });
+                } else if (confirm(message)) {
+                    action();
+                } else if (typeof instead === 'function') {
+                    instead();
+                }
+            };
+        });
+    }
+});
+
+
